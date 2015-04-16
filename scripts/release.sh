de16b64b5f19b1fe337245ea5f45bef9bade3756
Developer <frodo@workstation.com>
2015-04-16 18:41:09 -0400

    GPG key import fails if already added... ignore for now.

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,14 +9,16 @@ set -o pipefail
 
 sudo yum install -y gnupg
 
+set +e
 gpg --import /var/snap-ci/repo/tclemson.gpg.key
+set -e
 
 ./gradlew uploadArchives \
     -Psigning.keyID=$SIGNING_KEY_ID \
     -Psigning.password=$SIGNING_PASSWORD \
     -Psigning.secretKeyRingFile=$SIGNING_SECRET_KEY_RING_FILE
 
-export REPO_ID=$(./gradlew nexusStagingList | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
+REPO_ID=$(./gradlew nexusStagingList | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
 
 ./gradlew nexusStagingClose -PrepoId=$REPO_ID \
     -Poss-releases.username=$SONATYPE_USERNAME \
