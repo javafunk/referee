58745bba9c0df83d67b4ed3bfeb456332bad0aed
Developer <frodo@workstation.com>
2015-04-16 18:06:16 -0400

    Add release script.

---
+++ scripts/release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ "$DEBUG" == "true" ]]; then
+    set -x
+fi
+
+set -e
+
+yum install -y gnupg
+
+gpg --import /var/snap-ci/repo/tclemson.gpg.key
+
+./gradlew uploadArchives \
+    -Psigning.keyID=$SIGNING_KEY_ID \
+    -Psigning.password=$SIGNING_PASSWORD \
+    -Psigning.secretKeyRingFile=$SIGNING_SECRET_KEY_RING_FILE
+
+export REPO_ID=$(./gradlew nexusStagingList | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
+
+./gradlew nexusStagingClose -PrepoId=$REPO_ID \
+    -Poss-releases.username=$SONATYPE_USERNAME \
+    -Poss-releases.password=$SONATYPE_PASSWORD \
+    -Poss-releases.url=$SONATYPE_URL
+
+./gradlew nexusStagingPromote -PrepoId=$REPO_ID \
+    -Poss-releases.username=$SONATYPE_USERNAME \
+    -Poss-releases.password=$SONATYPE_PASSWORD \
+    -Poss-releases.url=$SONATYPE_URL
