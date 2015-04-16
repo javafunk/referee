efa49eb220f079069d523979fdf7194be1b69dde
Developer <frodo@workstation.com>
2015-04-16 18:52:23 -0400

    Add extra gradle properties to command in release script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,14 +9,17 @@ set -o pipefail
 
 sudo yum install -y gnupg
 
-set +e
-gpg --import /var/snap-ci/repo/tclemson.gpg.key
-set -e
+if !gpg --list-keys 44CE9D82; then
+    gpg --import /var/snap-ci/repo/tclemson.gpg.key
+fi
 
 ./gradlew uploadArchives \
     -Psigning.keyId=$SIGNING_KEY_ID \
     -Psigning.password=$SIGNING_PASSWORD \
-    -Psigning.secretKeyRingFile=$SIGNING_SECRET_KEY_RING_FILE
+    -Psigning.secretKeyRingFile=$SIGNING_SECRET_KEY_RING_FILE \
+    -Poss-releases.username=$SONATYPE_USERNAME \
+    -Poss-releases.password=$SONATYPE_PASSWORD \
+    -Poss-releases.url=$SONATYPE_URL
 
 REPO_ID=$(./gradlew nexusStagingList | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
 
