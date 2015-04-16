11f69539a2e6dbb94aadd36363163594f2469b22
Developer <frodo@workstation.com>
2015-04-16 19:16:51 -0400

    Try using properties file for gradle properties.

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,28 +13,21 @@ if ! gpg --list-keys 44CE9D82; then
     gpg --import /var/snap-ci/repo/tclemson.gpg.key
 fi
 
-./gradlew uploadArchives \
-    -Psigning.keyId=$SIGNING_KEY_ID \
-    -Psigning.password=$SIGNING_PASSWORD \
-    -Psigning.secretKeyRingFile=$SIGNING_SECRET_KEY_RING_FILE \
-    -Poss-releases.username=$SONATYPE_USERNAME \
-    -Poss-releases.password=$SONATYPE_PASSWORD \
-    -Poss-releases.url=$SONATYPE_URL
+echo <<EOF
+signing.keyId=$SIGNING_KEY_ID
+signing.password=$SIGNING_PASSWORD
+signing.secretKeyRingFile=$SIGNING_SECRET_KEY_RING_FILE
+oss-releases.username=$SONATYPE_USERNAME
+oss-releases.password=$SONATYPE_PASSWORD
+oss-releases.url=$SONATYPE_URL
+EOF > gradle.properties
 
-REPO_ID=$(./gradlew nexusStagingList \
-    -Poss-releases.username=$SONATYPE_USERNAME \
-    -Poss-releases.password=$SONATYPE_PASSWORD \
-    -Poss-releases.url=$SONATYPE_URL \
-    | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
+./gradlew uploadArchives
 
-./gradlew nexusStagingClose -PrepoId=$REPO_ID \
-    -Poss-releases.username=$SONATYPE_USERNAME \
-    -Poss-releases.password=$SONATYPE_PASSWORD \
-    -Poss-releases.url=$SONATYPE_URL
+REPO_ID=$(./gradlew nexusStagingList | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
+
+./gradlew nexusStagingClose -PrepoId=$REPO_ID
 
 sleep 1m
 
-./gradlew nexusStagingPromote -PrepoId=$REPO_ID \
-    -Poss-releases.username=$SONATYPE_USERNAME \
-    -Poss-releases.password=$SONATYPE_PASSWORD \
-    -Poss-releases.url=$SONATYPE_URL
+./gradlew nexusStagingPromote -PrepoId=$REPO_ID
