9ddf22666ba6d854e633d4c59eb7b63c4cc174c0
Developer <frodo@workstation.com>
2015-04-16 19:22:02 -0400

    Copy gradle properties from secure file.

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,21 +13,12 @@ if ! gpg --list-keys 44CE9D82; then
     gpg --import /var/snap-ci/repo/tclemson.gpg.key
 fi
 
-echo <<EOF
-signing.keyId=$SIGNING_KEY_ID
-signing.password=$SIGNING_PASSWORD
-signing.secretKeyRingFile=$SIGNING_SECRET_KEY_RING_FILE
-oss-releases.username=$SONATYPE_USERNAME
-oss-releases.password=$SONATYPE_PASSWORD
-oss-releases.url=$SONATYPE_URL
-EOF > gradle.properties
+cp /var/snap-ci/repo/gradle.properties .
 
 ./gradlew uploadArchives
 
 REPO_ID=$(./gradlew nexusStagingList | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
 
 ./gradlew nexusStagingClose -PrepoId=$REPO_ID
-
 sleep 1m
-
 ./gradlew nexusStagingPromote -PrepoId=$REPO_ID
