92a841cc6ccab9fe7621ab18683b8669c6961096
Developer <frodo@workstation.com>
2015-04-16 18:55:54 -0400

    Use nexusStagingRelease command instead in release script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,7 @@ set -o pipefail
 
 sudo yum install -y gnupg
 
-if !gpg --list-keys 44CE9D82; then
+if ! gpg --list-keys 44CE9D82; then
     gpg --import /var/snap-ci/repo/tclemson.gpg.key
 fi
 
@@ -21,14 +21,7 @@ fi
     -Poss-releases.password=$SONATYPE_PASSWORD \
     -Poss-releases.url=$SONATYPE_URL
 
-REPO_ID=$(./gradlew nexusStagingList | grep "Repository ID" | cut -d ' ' -f 3 | cut -d ',' -f  1)
-
-./gradlew nexusStagingClose -PrepoId=$REPO_ID \
-    -Poss-releases.username=$SONATYPE_USERNAME \
-    -Poss-releases.password=$SONATYPE_PASSWORD \
-    -Poss-releases.url=$SONATYPE_URL
-
-./gradlew nexusStagingPromote -PrepoId=$REPO_ID \
+./gradlew nexusStagingRelease \
     -Poss-releases.username=$SONATYPE_USERNAME \
     -Poss-releases.password=$SONATYPE_PASSWORD \
     -Poss-releases.url=$SONATYPE_URL
